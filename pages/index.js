10bd7f5d11dfe60ba9b3dcdc01ae624b2d281e6a
Atihar Hossen Mahir <mahir_1990@yahoo.com>
2022-09-09 17:09:23 +0400

    tailwind config

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+  
+const HomePage = () => {
+  return (
+    <div className='text-3xl text-teal-600 p-2'>
+     All restaurants
+    </div>
+  )
+}
+  
+export default HomePage
